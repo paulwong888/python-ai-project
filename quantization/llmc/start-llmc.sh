3551407101b1e20cd7f4275e56744757889299bf
Paul <paulwong888@163.com>
2025-01-19 20:59:30 +0800

    update

---
+++ quantization/llmc/start-llmc.sh
@@ -0,0 +1,9 @@
+docker compose down
+docker compose up -d
+docker exec -it llmc /bin/bash
+
+#conda activate torch231
+#You can use pip list in the container to check the packages installed.
+#You can find calib/eval datasets at /home/datasets/llmc.zip
+#cd /home/datasets
+#unzip llmc.zip
