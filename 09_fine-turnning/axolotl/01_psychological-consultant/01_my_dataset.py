1a96cdb0c02e12aa7062eba917fd8f997539a84d
paulwong888 <paulwong888@163.com>
2025-03-28 09:39:50 +0800

    update

--- 09_fine-turnning/axolotl/01_psychological-consultant/01_my_dataset.py
+++ 09_fine-turnning/axolotl/01_psychological-consultant/01_my_dataset.py
@@ -2,8 +2,13 @@ from datasets import load_dataset, Dataset
 
 class MyDataset():
     def __init__(self):
-        data_set_path = "/home/paul/.cache/huggingface/hub/datasets--Kedreamix--psychology-10k-Deepseek-R1-zh"
-        data_set = load_dataset(data_set_path, split="train")
+        data_set_path = "/root/workspaces/huggingface/datasets--Kedreamix--psychology-10k-Deepseek-R1-zh"
+        self.system_message = """Below is an instruction that describes a task, paired with an input that provides further context. 
+        Write a response that appropriately completes the request. 
+        Before answering, think carefully about the question and create a step-by-step chain of thoughts to ensure a logical and accurate response.
+        You are a medical expert with advanced knowledge in clinical reasoning, diagnostics, and treatment planning. 
+        """
+        data_set = load_dataset("json", data_dir=data_set_path, split="train")
         self.data_set: Dataset = self.pre_processing(data_set)#.select(range(5))
         self.save()
 
@@ -14,8 +19,10 @@ class MyDataset():
         all_messages = []
         for input, reasoning_content, content in zip(inputs, reasoning_contents, contents):
             messages = []
+            system_message = dict(role = "system", content = self.system_message)
             user_message = dict(role = "user",content = input)
             assiant_message = dict(role = "assistant", content = f"<think>{reasoning_content}</think>{content}")
+            messages.append(system_message)
             messages.append(user_message)
             messages.append(assiant_message)
             all_messages.append(messages)
